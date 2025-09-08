8c8a353b078ce95b151cf7731abd2ed0dca148bc
David Holder <david.holder@gmail.com>
2025-09-08 14:34:43 +0100

    Update extra-script.sh

--- gitops/extra-script.sh
+++ gitops/extra-script.sh
@@ -5,7 +5,8 @@ sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 # Install ArgoCD CLI
 VERSION=$(curl -L -s https://raw.githubusercontent.com/argoproj/argo-cd/stable/VERSION)
 curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/download/v$VERSION/argocd-linux-amd64
-sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+sudo install -o root -g root -m 0755 argocd-linux-amd64 /usr/local/bin/argocd
 
 # Install Kustomise CLI
 curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
+sudo install -o root -g root -m 0755 kustomise /usr/local/bin/kustomize
